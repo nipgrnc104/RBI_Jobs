c0d981bfe76c79a38bfd268c6bd8ad17c6674481
nipgrnc104 <nipgrnc104@gmail.com>
2016-12-26 13:46:40 +0530

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -6,18 +6,18 @@
 // require 'scraperwiki/simple_html_dom.php';
 //
 // // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
+ $html = scraperwiki::scrape("https://opportunities.rbi.org.in/Scripts/Vacancies.aspx");
 //
 // // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
+ $dom = new simple_html_dom();
+ $dom->load($html);
+ print_r($dom->find("table.list"));
 //
 // // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
+ scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
 //
 // // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
+ scraperwiki::select("* from data where 'name'='peter'")
 
 // You don't have to do things with the ScraperWiki library.
 // You can use whatever libraries you want: https://morph.io/documentation/php
